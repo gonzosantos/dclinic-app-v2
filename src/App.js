cf235d1e7f7bca7a41c539fbb2d2408737c234d6
gonzosantos <37707794+gonzosantos@users.noreply.github.com>
2022-12-02 03:43:06 +0000

    first failed tests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,38 @@
 import './App.css';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Parse from 'parse/react-native';
+
+//Before using the SDK...
+Parse.setAsyncStorage(AsyncStorage);
+
+//Saving your First Data Object on Back4App
+async function saveNewPerson() {
+  const person = new Parse.Object("Person");
+
+  person.set("name", "John Snow");
+  person.set("age", 27);
+  try {
+    let result = await person.save()
+    alert('New object created with objectId: ' + result.id);
+    } catch(error) {
+        alert('Failed to create new object, with error code: ' + error.message);
+    }
+  } 
+
+//Reading your First Data Object from Back4App
+async function retrievePerson() {
+  const query = new Parse.Query("Person");
+  
+  try {
+    const person = await query.get("mhPFDlCahj");
+    const name = person.get("name");
+    const age = person.get("age");
+  
+    alert(`Name: ${name} age: ${age}`);
+  } catch (error) {
+    alert(`Failed to retrieve the object, with error code: ${error.message}`);
+  }
+}  
 
 function App() {
   return (
