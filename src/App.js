33423e9bdcf75e356126b462ae34d231e837f90d
gonzosantos <37707794+gonzosantos@users.noreply.github.com>
2022-12-02 04:22:36 +0000

    first success.Use React+yarn (opsd 2 React Native)

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
 import './App.css';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import Parse from 'parse/react-native';
+import Parse from 'parse/dist/parse.min.js';
+import { PersonComponent } from './PersonComponent';
 
-//Before using the SDK...
-Parse.setAsyncStorage(AsyncStorage);
-
-//Saving your First Data Object on Back4App
-async function saveNewPerson() {
-  const person = new Parse.Object("Person");
-
-  person.set("name", "John Snow");
-  person.set("age", 27);
-  try {
-    let result = await person.save()
-    alert('New object created with objectId: ' + result.id);
-    } catch(error) {
-        alert('Failed to create new object, with error code: ' + error.message);
-    }
-  } 
-
-//Reading your First Data Object from Back4App
-async function retrievePerson() {
-  const query = new Parse.Query("Person");
-  
-  try {
-    const person = await query.get("mhPFDlCahj");
-    const name = person.get("name");
-    const age = person.get("age");
-  
-    alert(`Name: ${name} age: ${age}`);
-  } catch (error) {
-    alert(`Failed to retrieve the object, with error code: ${error.message}`);
-  }
-}  
+//Paste below the Back4App Application ID AND the JavaScript KEY
+Parse.initialize("qTaQkg7YBJezEmaKXPYvdYRcPGURyddS7eMOJVoj", "pWrCh2513z0GfE3an4y4b1s7cjc57UDfPGIlqa2E");
+//Point to Back4App Parse API address
+Parse.serverURL = 'https://parseapi.back4app.com/'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <PersonComponent />
       </header>
     </div>
   );
